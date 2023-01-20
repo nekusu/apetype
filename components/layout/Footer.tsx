3f621d962808ca6e497e8144c5f7a616b980a63b
nekusu <nekusu.dev@gmail.com>
2023-01-20 16:14:49 -0300

    feat(layout): add version button to footer (#3)

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { Button, Flex, Transition } from 'components/core';
-import { RiGithubLine } from 'react-icons/ri';
+import { Button, Flex, Tooltip, Transition } from 'components/core';
+import project from 'package.json';
+import { RiGitBranchLine, RiGithubLine } from 'react-icons/ri';
 
 export default function Footer() {
   return (
@@ -18,6 +19,19 @@ export default function Footer() {
             <RiGithubLine />
             nekusu
           </Button>
+        </Flex>
+        <Flex className='gap-1'>
+          <Tooltip label='See changelog' offset={10}>
+            <Button
+              className='p-0 text-sm'
+              component='a'
+              href={`https://github.com/nekusu/apetype/releases/tag/v${project.version}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <RiGitBranchLine />v{project.version}
+            </Button>
+          </Tooltip>
         </Flex>
       </Flex>
     </Transition>
