8f783f42b285bccd54d488d20be1501e0486d6ed
nekusu <nekusu.dev@gmail.com>
2023-02-19 20:15:36 -0300

    chore: edit eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+};
