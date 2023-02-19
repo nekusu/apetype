b7d89a0ec09259843f6c656d4f0f81aefd13b8c2
nekusu <nekusu.dev@gmail.com>
2023-02-19 20:13:27 -0300

    chore: edit release-it config

---
+++ .release-it.js
@@ -0,0 +1,43 @@
+module.exports = {
+  git: {
+    commitMessage: 'chore(release): v${version}',
+    tagName: 'v${version}',
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    release: true,
+    releaseName: 'v${version}',
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      infile: 'CHANGELOG.md',
+      header: '# Changelog',
+      preset: {
+        name: 'conventionalcommits',
+        types: [
+          {
+            type: 'feat',
+            section: 'New Features',
+          },
+          {
+            type: 'perf',
+            section: 'Performance Improvements',
+          },
+          {
+            type: 'fix',
+            section: 'Bug Fixes',
+          },
+        ],
+      },
+      writerOpts: {
+        commitGroupsSort: (a, b) => {
+          const sections = ['New Features', 'Performance Improvements', 'Bug Fixes'];
+          return sections.indexOf(a.title) - sections.indexOf(b.title);
+        },
+        commitsSort: 'header',
+      },
+    },
+  },
+};
