eaabac0493b127d12f003e75e6b9687d8eca806f
nekusu <nekusu.dev@gmail.com>
2023-04-07 20:03:44 -0300

    feat: add toCamelCase function util

--- utils/misc.ts
+++ utils/misc.ts
@@ -7,3 +7,9 @@ export function getRandomNumber(max = 1, min = 0) {
 export function replaceSpaces(string: string, replaceString = '_') {
   return string.replace(/ /g, replaceString);
 }
+
+export function toCamelCase(str: string): string {
+  return str.replace(/[^a-zA-Z0-9]+(.)/g, (_match, group1: string) => {
+    return group1.toUpperCase();
+  });
+}
