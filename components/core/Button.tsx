e718a0c0c76a087e5f08d5fed35dd39e71536be2
nekusu <nekusu.dev@gmail.com>
2023-04-07 18:01:53 -0300

    refactor(core): add 'danger' variant to Button

--- components/core/Button.tsx
+++ components/core/Button.tsx
@@ -1,12 +1,31 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { createPolymorphicComponent, PolymorphicComponentProps } from 'utils/polymorphicComponent';
+import { PolymorphicComponentProps, createPolymorphicComponent } from 'utils/polymorphicComponent';
 import Dynamic from './Dynamic';
 
 export interface ButtonProps
   extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   active?: boolean;
-  variant?: 'filled' | 'subtle' | 'text';
+  variant?: 'danger' | 'filled' | 'subtle' | 'text';
+}
+
+function getVariantStyles(variant: NonNullable<ButtonProps['variant']>) {
+  const className = ['danger', 'filled'].includes(variant)
+    ? 'bg-sub-alt text-text outline-offset-[-2px] active:scale-[.925]'
+    : 'text-sub hover:text-text';
+  switch (variant) {
+    case 'danger':
+      return twMerge([
+        className,
+        'outline-2 outline-error hover:bg-error hover:text-bg focus:bg-error focus:text-bg',
+      ]);
+    case 'filled':
+      return twMerge([className, 'hover:bg-text hover:text-bg focus:outline-2 focus:outline-text']);
+    case 'subtle':
+      return twMerge([className, 'focus:bg-text focus:text-bg active:scale-[.925]']);
+    case 'text':
+      return twMerge([className, 'focus:text-text active:-translate-y-0.5']);
+  }
 }
 
 const Button = forwardRef<HTMLButtonElement, PolymorphicComponentProps<'button', ButtonProps>>(
@@ -16,12 +35,8 @@ const Button = forwardRef<HTMLButtonElement, PolymorphicComponentProps<'button',
         component='button'
         className={twMerge([
           'flex w-max cursor-pointer select-none items-center justify-center gap-1.5 rounded-lg p-2 text-center text-base leading-tight outline-none transition',
-          variant === 'filled'
-            ? 'bg-sub-alt text-text outline-offset-[-2px] hover:bg-text hover:text-bg focus:outline-2 focus:outline-text active:scale-[.925]'
-            : 'text-sub hover:text-text focus:text-text',
+          getVariantStyles(variant),
           variant === 'filled' && active && 'bg-main text-bg',
-          variant === 'subtle' && 'focus:bg-text focus:text-bg active:scale-[.925]',
-          variant === 'text' && 'active:translate-y-[2px]',
           variant === 'text' && active && 'text-main',
           className,
         ])}
