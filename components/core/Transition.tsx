6ac9a152f171add29682ca90291f00a80731d826
nekusu <nekusu.dev@gmail.com>
2023-04-09 03:25:21 -0300

    refactor: improve component types

--- components/core/Transition.tsx
+++ components/core/Transition.tsx
@@ -1,23 +1,23 @@
 import { HTMLMotionProps, motion, Variants } from 'framer-motion';
-import { forwardRef } from 'react';
+import { ElementRef, forwardRef } from 'react';
 
 const DEFAULT_VARIANTS: Variants = {
   hidden: { opacity: 0, pointerEvents: 'none' },
   visible: { opacity: 1, pointerEvents: 'auto' },
 };
 
-const Transition = forwardRef<HTMLDivElement, HTMLMotionProps<'div'>>(function Transition(
+const Transition = forwardRef<ElementRef<'div'>, HTMLMotionProps<'div'>>(function Transition(
   { variants = DEFAULT_VARIANTS, ...props },
   ref
 ) {
   return (
     <motion.div
+      ref={ref}
       variants={variants}
       initial={variants.hidden ? 'hidden' : 'initial'}
       animate={variants.visible ? 'visible' : 'animate'}
       exit={variants.hidden ? 'hidden' : 'exit'}
       transition={{ duration: 0.15 }}
-      ref={ref}
       {...props}
     />
   );
