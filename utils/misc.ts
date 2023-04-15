61ebc6cc0acdd0e63487c17109132dd0aea6bd84
nekusu <nekusu.dev@gmail.com>
2023-04-15 15:06:44 -0300

    feat: add misc utils

--- utils/misc.ts
+++ utils/misc.ts
@@ -13,3 +13,29 @@ export function toCamelCase(str: string): string {
     return group1.toUpperCase();
   });
 }
+
+export function formatFileSize(bytes: number, si = false, decimalPlaces = 1) {
+  const threshold = si ? 1000 : 1024;
+  if (Math.abs(bytes) < threshold) return `${bytes} B`;
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  const round = 10 ** decimalPlaces;
+  let unitIndex = -1;
+  while (Math.round(Math.abs(bytes) * round) / round >= threshold && unitIndex < units.length - 1) {
+    bytes /= threshold;
+    unitIndex++;
+  }
+
+  return `${bytes.toFixed(decimalPlaces)} ${units[unitIndex]}`;
+}
+
+export function getLocalStorageSize(...keys: string[]) {
+  let totalSize = 0;
+  for (const key of keys) {
+    const item = localStorage.getItem(key);
+    if (!item) continue;
+    totalSize += (item.length + key.length) * 2;
+  }
+  return totalSize;
+}
