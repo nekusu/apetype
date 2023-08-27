2dcb1405476298af8166b96a03d9f4c9eda93c54
nekusu <nekusu.dev@gmail.com>
2023-08-27 00:30:33 -0300

    feat: add `ParallelRouteModal` component

---
+++ components/layout/ParallelRouteModal.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Modal } from 'components/core';
+import { ModalProps } from 'components/core/Modal';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
+
+export interface ParallelRouteModalProps extends Omit<ModalProps, 'open' | 'onClose'> {
+  routes: string[];
+}
+
+export default function ParallelRouteModal({
+  children,
+  routes,
+  ...props
+}: ParallelRouteModalProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const lastPathname = useRef('/');
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const open = routes.some((r) => pathname.includes(r));
+    setOpen(open);
+    if (!open) lastPathname.current = pathname;
+  }, [pathname, routes]);
+
+  return (
+    <Modal
+      centered
+      open={open}
+      onClose={() => router.push(lastPathname.current)}
+      layout
+      transition={{ type: 'spring', bounce: 0.6, layout: { type: 'spring', duration: 0.25 } }}
+      style={{ borderRadius: 8 }}
+      {...props}
+    >
+      {children}
+    </Modal>
+  );
+}
