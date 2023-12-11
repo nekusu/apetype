969c1837d627063e81212a274558142c3edfb49e
nekusu <nekusu.dev@gmail.com>
2023-12-11 09:55:46 -0300

    fix: last 10 tests average wpm calculation

--- components/profile/UserStats.tsx
+++ components/profile/UserStats.tsx
@@ -2,44 +2,40 @@
 
 import { useIntersection, useIsomorphicEffect } from '@mantine/hooks';
 import { useGetDocs } from '@tatsuokaniwa/swr-firestore';
-import { TypingTest, User } from 'context/userContext';
 import { useMemo, useRef, useState } from 'react';
+import { RiLoaderLine } from 'react-icons/ri';
+import { TypingTest, User } from 'utils/user';
 
-export interface UserStatsProps {
-  userId?: string;
-  completedTests?: number;
-  highest?: User['typingStats']['highest'];
-  average?: User['typingStats']['average'];
-}
+export type UserStatsProps =
+  | { user: User; highest?: undefined; average?: undefined }
+  | ({ user?: undefined } & Pick<User['typingStats'], 'highest' | 'average'>);
 
 const STAT_LABELS = ['wpm', 'raw', 'accuracy', 'consistency'] as const;
 
-export default function UserStats({
-  userId,
-  completedTests = 0,
-  highest,
-  average,
-}: UserStatsProps) {
+export default function UserStats({ user, ...props }: UserStatsProps) {
+  const { highest, average } = user?.typingStats ?? props;
+  const { completedTests = 0 } = user?.typingStats ?? {};
   const tableRef = useRef<HTMLTableElement>(null);
   const { ref, entry } = useIntersection({ root: tableRef.current, threshold: 0.1 });
   const [onScreen, setOnScreen] = useState(false);
-  const { data: lastTests } = useGetDocs<TypingTest>(
-    userId && completedTests > 10 && onScreen
-      ? { path: `users/${userId}/tests`, orderBy: [['date', 'desc']], limit: 10 }
+  const { data: lastTests, isLoading } = useGetDocs<TypingTest>(
+    user && completedTests > 10 && onScreen
+      ? { path: `users/${user.id}/tests`, orderBy: [['date', 'desc']], limit: 10 }
       : null,
+    { revalidateIfStale: false },
   );
   const averageLast = useMemo<UserStatsProps['average']>(() => {
-    return lastTests?.reduce(
-      (stats, test, index) => {
-        Object.keys(stats).map((_key) => {
-          const key = _key as keyof typeof stats;
-          stats[key] += test.result[key];
-          if (index > 0) stats[key] /= 2;
-        });
-        return stats;
-      },
-      { wpm: 0, raw: 0, accuracy: 0, consistency: 0 },
-    );
+    if (!lastTests) return;
+    const stats = { wpm: 0, raw: 0, accuracy: 0, consistency: 0 };
+    type Key = keyof typeof stats;
+    lastTests.forEach((test) => {
+      for (const _key in stats) {
+        const key = _key as Key;
+        stats[key] += test.result[key];
+      }
+    });
+    for (const key in stats) stats[key as Key] /= lastTests.length;
+    return stats;
   }, [lastTests]);
 
   useIsomorphicEffect(() => {
@@ -57,7 +53,7 @@ export default function UserStats({
           <th className='py-3 font-normal' scope='col'>
             average
           </th>
-          {averageLast && (
+          {completedTests > 10 && (
             <th
               className='flex flex-col items-center justify-center gap-1 py-2 font-normal leading-none'
               scope='col'
@@ -72,28 +68,33 @@ export default function UserStats({
         {STAT_LABELS.map((label) => {
           const showPercentage = ['accuracy', 'consistency'].includes(label) && '%';
           return (
-            <tr
-              key={label}
-              className='text-center text-3xl text-text transition-colors odd:bg-sub-alt'
-            >
+            <tr key={label} className='group text-center text-3xl text-text transition-colors'>
               <td
-                className='w-0 bg-bg py-4 pr-8 text-left text-2xl text-sub transition-colors'
+                className='w-0 py-4 pr-8 text-left text-2xl text-sub transition-colors'
                 scope='row'
               >
                 {label}
               </td>
-              <td className='rounded-l-xl'>
+              <td className='rounded-l-xl group-odd:bg-sub-alt'>
                 {highest?.[label]?.toFixed(2)}
                 {showPercentage}
               </td>
-              <td className='last:rounded-r-xl'>
+              <td className='last:rounded-r-xl group-odd:bg-sub-alt'>
                 {average?.[label]?.toFixed(2)}
                 {showPercentage}
               </td>
-              {averageLast && (
-                <td className='rounded-r-xl'>
-                  {averageLast?.[label]?.toFixed(2)}
-                  {showPercentage}
+              {completedTests > 10 && (
+                <td className='rounded-r-xl group-odd:bg-sub-alt'>
+                  {isLoading ? (
+                    <span className='flex justify-center'>
+                      <RiLoaderLine className='animate-spin' />
+                    </span>
+                  ) : (
+                    <>
+                      {averageLast?.[label]?.toFixed(2)}
+                      {showPercentage}
+                    </>
+                  )}
                 </td>
               )}
             </tr>
